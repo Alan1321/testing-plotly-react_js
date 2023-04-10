6066ae358b0b76888f6f576824b0c7cb71d879b4
alansubedi123 <alansubedi123@gmail.com>
2023-04-10 12:17:23 -0500

    Init Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import Plot from 'react-plotly.js';
+
+const getData = () =>{
+  var Plotly = require('plotly.js-dist-min')
+  var arr = []
+  for(let i=0;i<10;i++){
+    arr.push(Array(3).fill().map(()=>Math.random()))
+  }
+  
+  Plotly.newPlot('chart',[{
+    z:arr,
+    type:'surface'
+  }])
+}
 
 function App() {
+  
+  var data = getData()
+  const type = 'scatter3d'
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {console.log(data)}
     </div>
   );
 }
